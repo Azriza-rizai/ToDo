844818f21ddcb51a647b197895a67bc34ee7b8f9
Azriza-rizai <azriza.rizai@gmail.com>
2025-04-30 20:55:35 +0200

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,43 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Counter from './Counter'
+
+ const initialTodo=[
+  { 
+    id: 1,
+    task:"learn React", 
+    completed: true
+  },
+  { 
+    id: 2,
+    task:"Go Shopping", 
+    completed: false
+  },
+  { 
+    id: 3,
+    task:"Go to gym", 
+    completed: true
+  }
+]
 
 function App() {
+
+  
 
 
   return (
     <>
-     
-        <h1>Fitness App </h1> 
-        <Counter label="cardio training"></Counter>
-        <Counter label="weight training"></Counter>
-        <Counter label="water training"></Counter>
+    <h1>Todo App</h1>
+    <ul>
+      {initialTodo.map(todo => (
+        <li>{todo.task}</li>
+      ))}
+    </ul>
+
+  <form>
+    <input type= "text"></input>
+    <button type= "submit">submit</button>
+  </form>
     </>
   )
 }
